9b87bab20d5110288194d305f35ca272a20a680a
Alexander Andryashin <aandryashin@gmail.com>
2018-03-25 14:40:23 +0300

    Added JsonError function

--- util.go
+++ util.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"encoding/json"
 	"net"
 	"net/http"
 	"net/url"
@@ -9,6 +10,18 @@ import (
 
 func SecondsSince(start time.Time) float64 {
 	return float64(time.Now().Sub(start).Seconds())
+}
+
+func JsonError(w http.ResponseWriter, msg string, code int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(
+		map[string]interface{}{
+			"value": map[string]string{
+				"message": msg,
+			},
+			"status": 13,
+		})
 }
 
 func RequestInfo(r *http.Request) (string, string) {
